c8c6aba522ed5e2eda9d071b1d9eb0fc878043cf
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2023-01-12 12:13:41 +0000

    initial

---
+++ theme.config.tsx
@@ -0,0 +1,107 @@
+import { DocsThemeConfig, useConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router'
+
+const logo = (
+  <span>
+    PL
+  </span>
+)
+
+const config: DocsThemeConfig = {
+  project: {
+    link: 'https://github.com/shuding/nextra'
+  },
+  docsRepositoryBase: 'https://github.com/shuding/nextra/tree/main/docs',
+  useNextSeoProps() {
+    const { route } = useRouter()
+    if (route !== '/') {
+      return {
+        titleTemplate: '%s – Nextra'
+      }
+    }
+  },
+  logo,
+  head: () => {
+    const { title } = useConfig()
+    const { route } = useRouter()
+    const socialCard =
+      route === '/' || !title
+        ? 'https://nextra.site/og.jpeg'
+        : `https://nextra.site/api/og?title=${title}`
+
+    return (
+      <>
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="Content-Language" content="en" />
+        <meta
+          name="description"
+          content="Make beautiful websites with Next.js & MDX."
+        />
+        <meta
+          name="og:description"
+          content="Make beautiful websites with Next.js & MDX."
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content={socialCard} />
+        <meta name="twitter:site:domain" content="nextra.site" />
+        <meta name="twitter:url" content="https://nextra.site" />
+        <meta
+          name="og:title"
+          content={title ? title + ' – Nextra' : 'Nextra'}
+        />
+        <meta name="og:image" content={socialCard} />
+        <meta name="apple-mobile-web-app-title" content="Nextra" />
+        
+        <link rel="icon" href="/favicon.png" type="image/png" />
+
+      </>
+    )
+  },
+  // banner: {
+  //   key: '2.0-release',
+  //   text: (
+  //     <a href="https://nextra.site" target="_blank" rel="noreferrer">
+  //       🎉 Nextra 2.0 is released. Read more →
+  //     </a>
+  //   )
+  // },
+  editLink: {
+    text: 'Edit this page on GitHub →'
+  },
+  feedback: {
+    content: () => <>Question? Give us feedback →</>,
+    labels: 'feedback'
+  },
+  sidebar: {
+    titleComponent: ({ title, type }) => {
+      if (type === 'separator') {
+        return <span className="cursor-default">{title}</span>
+      }
+      return <>{title}</>
+    },
+    defaultMenuCollapseLevel: 0
+  },
+  footer: {
+    text: () => {
+      return (
+        <div className="flex w-full flex-col items-center sm:items-start">
+          <div>
+            <a
+              className="flex items-center gap-1 text-current"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="vercel.com homepage"
+              href="https://vercel.com?utm_source=nextra.site"
+            >
+            </a>
+          </div>
+          <p className="mt-6 text-xs">© 2022 Casiano Rodríguez León.</p>
+        </div>
+      )
+    }
+  }
+}
+
+export default config
