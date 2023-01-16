1383037a0cb01f0af38ff9c78c6e5d4f955b0978
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2023-01-16 19:58:04 +0000

    setting environment variables on the serverless function

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,8 +1,10 @@
 export default async function (req, res) {
   console.log(req.query);
   const {a, b} = req.query;
+  const chuchu = Number(process.env.CHUCHU);
   res.status(200).json({ 
-    sum: Number(a)+Number(b), 
+    sum: Number(a)+Number(b),
+    chuchu: chuchu, 
     comment: "hello world!. This is running on the server!",
     ...req.query 
   });
