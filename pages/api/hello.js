b5acd35a2d6e7fbfa0123479161976ace06cfa25
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2023-01-16 17:01:28 +0000

    nextra pages/api example

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,4 +1,9 @@
 export default async function (req, res) {
-  console.log(req.query)
-  res.status(200).json({ result: "hello world!. This is running on the server!" });
+  console.log(req.query);
+  const {a, b} = req.query;
+  res.status(200).json({ 
+    sum: Number(a)+Number(b), 
+    comment: "hello world!. This is running on the server!",
+    ...req.query 
+  });
 }
